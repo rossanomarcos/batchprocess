6ca8349d7363de68d9775538acc9735a8d86aaff
Rajat Ratewal <rajatratewal@gmail.com>
2020-06-11 13:44:28 +0530

    changed for small dataset

--- dags/transform/genome_tags.scala
+++
@@ -1,3 +0,0 @@
-val genome_tags_df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("s3://<s3-bucket>/genome-tags.csv")
-
-genome_tags_df.write.mode("overwrite").parquet("s3://<s3-bucket>/movielens-parquet/genome-tags/")
